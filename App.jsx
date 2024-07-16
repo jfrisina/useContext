7166d820411c63996f0f58fa259da4f03a6e2772
Jaki Frisina <32113890+jfrisina@users.noreply.github.com>
2024-07-16 13:16:39 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,61 @@
+import { useState, useContext } from "react";
+import { ImageContext } from "./Context.js";
+import { places } from "./data.js";
+import { getImageUrl } from "./utils.js";
+import ContextProvider from "./Context.js";
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+  return (
+    <>
+      <ContextProvider imageSize={imageSize}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={(e) => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List />
+      </ContextProvider>
+    </>
+  );
+}
+
+function List() {
+  const listItems = places.map((place) => (
+    <li key={place.id}>
+      <Place place={place} />
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place }) {
+  return (
+    <>
+      <PlaceImage place={place} />
+      <p>
+        <b>{place.name}</b>
+        {": " + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const { imageSize } = useContext(ImageContext);
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}
